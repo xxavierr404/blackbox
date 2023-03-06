e51e587ed4e2c01f71f772e5c617063ce90fa1ce
Vasiliev Victor <vasilievvict1@yandex.ru>
2023-03-06 19:11:23 +0300

    Created lava bullets

---
+++ Assets/Scripts/Objects/LavaBullet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Objects
+{
+    public class LavaBullet: MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            var freezable = collision.gameObject.GetComponent<Freezable>();
+            if (freezable)
+            {
+                freezable.ResetFreezeRate();
+            }
+
+            Destroy(this);
+        }
+    }
+}
