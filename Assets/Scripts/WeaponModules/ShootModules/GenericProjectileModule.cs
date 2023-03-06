49eb0cb33323bd808ddd9a25acfe1943f6f6109c
Vasiliev Victor <vasilievvict1@yandex.ru>
2023-03-06 19:19:02 +0300

    Created generic projectile shooting module

---
+++ Assets/Scripts/WeaponModules/ShootModules/GenericProjectileModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace WeaponModules.ShootModules
+{
+    public class GenericProjectileModule : ShootModule
+    {
+        [SerializeField] private GameObject bullet;
+        [SerializeField] private float shootingForce;
+        [SerializeField] private float shootDelay;
+
+        private float _delay;
+
+        private void Start()
+        {
+            _delay = 0;
+        }
+
+        private void Update()
+        {
+            if (_delay > 0)
+            {
+                _delay -= Time.deltaTime;
+            }
+        }
+
+        public override void Shoot(Transform shooter)
+        {
+            if (_delay > 0)
+            {
+                return;
+            }
+
+            base.Shoot(shooter);
+            var bulletInstance = Instantiate(bullet, transform, false);
+            bulletInstance.GetComponent<Rigidbody>().AddForce(transform.forward * shootingForce, ForceMode.Impulse);
+            _delay = shootDelay;
+        }
+    }
+}
