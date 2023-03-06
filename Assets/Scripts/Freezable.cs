bcff8c82f91d34d1473692281ed73aba733ce8fb
Vasiliev Victor <vasilievvict1@yandex.ru>
2023-03-06 18:38:49 +0300

    Changing color of objects during freeze

--- Assets/Scripts/Freezable.cs
+++ Assets/Scripts/Freezable.cs
@@ -6,13 +6,41 @@ public class Freezable : MonoBehaviour
     [SerializeField] private float maxFreezeRate;
     [SerializeField] private float freezeDuration;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private new MeshRenderer renderer;
 
     private float _currentFreezeRate;
+    private float CurrentFreezeRate
+    {
+        get => _currentFreezeRate;
+        set
+        {
+            if (renderer)
+            {
+                if (value == 0)
+                {
+                    renderer.material.color = _initialColor;
+                }
+                else
+                {
+                    var material = renderer.material;
+                    var oldColor = material.color;
+                    material.color = new Color(
+                        oldColor.r * (maxFreezeRate - value) / maxFreezeRate,
+                        oldColor.g * (maxFreezeRate - value) / maxFreezeRate,
+                        value / maxFreezeRate * 255);
+                }
+            }
+
+            _currentFreezeRate = value;
+        }
+    }
     private float _timePassed;
     private bool _frozen;
+    private Color _initialColor;
     
     private void Start()
     {
+        _initialColor = renderer.material.color;
         ResetFreezeRate();
     }
 
@@ -30,15 +58,15 @@ public class Freezable : MonoBehaviour
 
     public void ResetFreezeRate()
     {
-        _currentFreezeRate = 0;
+        CurrentFreezeRate = 0;
         Unfreeze();
     }
     
     public void IncreaseFreezeRate(float freezeRate)
     {
         _timePassed = 0;
-        _currentFreezeRate += freezeRate;
-        if (_currentFreezeRate >= maxFreezeRate)
+        CurrentFreezeRate += freezeRate;
+        if (CurrentFreezeRate >= maxFreezeRate)
         {
             Freeze();
         }
