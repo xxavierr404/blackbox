07081f765e5cc17ad8e95f7915911011533ce961
Vasiliev Victor <vasilievvict1@yandex.ru>
2023-03-06 21:11:07 +0300

    Added and tested teleport module

---
+++ Assets/Scripts/WeaponModules/ShootModules/TeleportModule.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace WeaponModules.ShootModules
+{
+    public class TeleportModule : ShootModule
+    {
+        [SerializeField] private Transform teleportableTransform;
+        
+        public override void Shoot(Transform shooter)
+        {
+            base.Shoot(shooter);
+            if (!CanShoot) return;
+
+            RaycastHit hit;
+            if (!Physics.Raycast(shooter.position, shooter.forward, out hit, 20f))
+            {
+                return;
+            }
+
+            teleportableTransform.position = hit.transform.position + Vector3.up;
+        }
+    }
+}
